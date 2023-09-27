a25da611bcce48d03cb0a7595060c6732da4c37a
woehrlej <woehrlej@amazon.de>
2023-09-27 09:16:25 +0200

    Add Toolchain plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,6 +15,10 @@
 
 rootProject.name = "sfc"
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+}
+
 listOf("core", "metrics", "adapters", "targets", "examples").forEach { p ->
     File("$rootDir/$p/").listFiles()?.forEach {
         if (it.isDirectory && File(it, "build.gradle.kts").exists()) {
