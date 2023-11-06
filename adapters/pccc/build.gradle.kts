da64aabbeba87b4b40387c93375c0626316cb63c
Arie Leeuwesteijn <leeuwest@amazon.com>
2023-11-06 16:57:05 +0100

    MIT-0 license headers

--- adapters/pccc/build.gradle.kts
+++ adapters/pccc/build.gradle.kts
@@ -1,18 +1,10 @@
 import org.jetbrains.kotlin.util.capitalizeDecapitalize.toUpperCaseAsciiOnly
 import java.time.LocalDate
 
-/*
- *Copyright(c) 2023. Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * Licensed under the Amazon Software License (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is located at :
- *
- *     http://aws.amazon.com/asl/
- *
- * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific
- * language governing permissions and limitations under the License.
- *
- */
+
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
 
 group = "com.amazonaws.sfc"
 version = "1.0.0"
