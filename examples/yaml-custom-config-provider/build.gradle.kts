0bb374c5efad304f61686ad1243c130329c1d8f4
Arie Leeuwesteijn <leeuwest@amazon.com>
2023-12-18 16:31:27 +0100

    YAML config example

---
+++ examples/yaml-custom-config-provider/build.gradle.kts
@@ -0,0 +1,23 @@
+group = "com.amazonaws.sfc"
+
+version = "1.0.0"
+
+val sfcCoreVersion = "1.0.0"
+val kotlinCoroutinesVersion = "1.6.2"
+val kotlinVersion = "1.9.0"
+
+plugins {
+
+    java
+
+    id("sfc.kotlin-library-conventions")
+}
+
+dependencies {
+
+    implementation(project(":core:sfc-core"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
+
+
+}
+
